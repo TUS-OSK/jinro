63d4def5d0a40908f17f068103014671eed55f0d
Fkeito <kei1229tomi@gmail.com>
2019-10-18 17:30:31 +0900

    テトラアンミン同作隊

---
+++ Assets/Scripts/View.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class View : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        OnCreate();
+    }
+    private void OnDisable()
+    {
+        OnClose();
+    }
+    private void OnDestroy()
+    {
+        
+    }
+
+    protected virtual void OnCreate()
+    {
+        Debug.Log("View is created!");
+    }
+    protected virtual void OnClose()
+    {
+        Debug.Log("View is closed!");
+    }
+}
---
+++ Assets/Scripts/ViewManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewManager : MonoBehaviour
+{
+    public static ViewManager instance;
+
+    private List<View> views;
+
+    public Transform root;
+    public string firstView;
+
+    public void BackView()
+    {
+        if (views.Count > 1)
+        {
+            views.RemoveAt(views.Count - 1);
+            views[views.Count - 1].gameObject.SetActive(true);
+        }
+    }
+    public void StartView(string path)
+    {
+        GameObject viewObj = Resources.Load(path) as GameObject;
+        viewObj.transform.parent = root;
+        View view = viewObj?.GetComponent<View>();
+
+        if (view)
+        {
+            if(views.Count > 0) views[views.Count - 1].gameObject.SetActive(false);
+            views.Add(view);
+        }
+        else
+        {
+            DestroyImmediate(viewObj);
+            Debug.LogError(view + "doesnt have View component");
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance) DestroyImmediate(this);
+
+        instance = this;
+        StartView(firstView);
+    }
+}
